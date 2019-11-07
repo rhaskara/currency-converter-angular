943d959c303d453693ff681fead489246e6f7396
Rafferty Hutari Askara <rafferty.askara@gmail.com>
2019-11-07 14:13:52 -0800

    heroku deployment test

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
-
-app.use(cors());
 
 app.use(express.static(path.join(__dirname + 'public')));
 
