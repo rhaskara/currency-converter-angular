cf7cb99771f9feaf8c2e7dda5f5dbf967a8e4449
Rafferty Hutari Askara <rafferty.askara@gmail.com>
2019-11-07 14:40:12 -0800

    express fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 
