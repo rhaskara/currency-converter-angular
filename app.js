4d6d42fa3fd02d34708b7c37f12e03a6d986d443
Rafferty Hutari Askara <rafferty.askara@gmail.com>
2019-11-07 13:52:57 -0800

    modified app.js and package.json

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}) );
 
-app.use( express.static(__dirname + '/client' ) );
+app.get('/', function (req, res) {
+  res.send('Invalid end point');
+});
 
-var listener = server.listen(process.env.PORT || 5000, function(){
-    console.log('Listening on port ' + listener.address().port); //Listening on port 5000
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port, function(){
+    console.log('Server started on port ' + port);
 });
